6cbedc0981234050280f4d4ee92f4a2978b2c9c6
v1lliam <zotovgr@yandex.ru>
2022-04-16 21:01:36 +0300

    day 1

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,7 +1,8 @@
 package com.example.servingwebcontent;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Map<String, Object> model) {
+		model.put("name", name);
 		return "greeting";
+	}
+
+	@GetMapping
+	public String main(Map<String, Object> model) {
+		model.put("some", "Hello, It's just try!");
+		return "main";
 	}
 
 }
--- complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -1,5 +1,7 @@
 package com.example.servingwebcontent;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
